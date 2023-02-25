0b131add9d94d2df2ee72578bb76b15dc35a1b47
yanmeng26 <myan001@ucr.edu>
2023-02-25 16:11:04 -0700

    added directory

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Directory from "./components/directory/directory.component";
+const App = () => {
+
+  const categories = [{
+    id:1,
+    title:'Springs',
+    imageUrl: "https://i.ibb.co/4YqNv57/pexels-ben-mack-6775389.jpg"
+  },
+  {
+    id:2,
+    title:'Summer',
+    imageUrl:"https://i.ibb.co/mbYrpzD/pexels-jesse-zheng-732548.jpg"
+  },
+  {
+    id:3,
+    title:'Fall',
+    imageUrl:"https://i.ibb.co/xGSPwhc/pexels-digital-buggu-319382.jpg" 
+  },
+  {
+    id:4,
+    title:'Winter',
+    imageUrl:"https://i.ibb.co/ChP5HPq/pexels-irina-iriser-1122639.jpg"
+  },
+  {
+    id:5,
+    title:'All Season',
+    imageUrl:"https://i.ibb.co/GCsncnR/harli-marten-n7a2-OJDSZns-unsplash.jpg"
+  }
+]
+return (
+  <Directory categories = {categories} />
+);
+};
 
 export default App;
