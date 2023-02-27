69783647c283dba5826b9e1c411fd0985da8cd38
yanmeng26 <myan001@ucr.edu>
2023-02-26 19:22:16 -0700

    retry deploy

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,7 +1,7 @@
 import { createContext, useState, useEffect } from 'react';
-import SHOP_DATA from '../shop-data.js';
+// import SHOP_DATA from '../shop-data.js';
 import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
-import { addCollectionAndDocuments } from '../utils/firebase/firebase.utils';
+// import { addCollectionAndDocuments } from '../utils/firebase/firebase.utils';
 
 export const CategoriesContext = createContext({
   categoriesMap: {},
